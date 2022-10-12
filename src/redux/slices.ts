d35e047752ec31add6417ccb09db830d33d91020
Anuoluwapo <63651250+Owoade@users.noreply.github.com>
2022-10-11 18:18:58 -0700

    Completed shoping cart functionality

--- src/redux/slices.ts
+++ src/redux/slices.ts
@@ -14,6 +14,7 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     addItem(state, action: PayloadAction<ICartitem>) {
+        
       if (
         state.items.some(
           (item) =>
